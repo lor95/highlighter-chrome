4440b1e91cc6a2640e3b5b9f775ad763c28384e3
Lorenzo Giuliani <lory_extr3me@hotmail.it>
2021-05-09 20:19:02 +0200

    fixes

--- src/content.js
+++ src/content.js
@@ -2,31 +2,26 @@ var storageCache = {}
 const def_class = "highlighter_chrome-ext"
 
 document.addEventListener('mouseup', function () {
-    if (storageCache.can_exec) {
-        sel = window.getSelection();
-        if (sel.toString().length > 0) {
-            var sel_r = sel.getRangeAt(0)
-            try {
-                var elements = sel_r.commonAncestorContainer.getElementsByTagName("*");
-                debugger
-            } catch {
-                var e = sel_r.startContainer;
-                for (var elem of e.parentElement.childNodes) {
-                    if (e.isSameNode(elem)) {
-                        if (!Array.from(e.parentElement.classList).includes(def_class)) {
-                            var bef_node = e.textContent.slice(0, sel_r.startOffset);
-                            var end_node = e.textContent.slice(sel_r.endOffset);
-                            var node = document.createElement('span');
-                            var val = (new Date().valueOf()).toString();
-                            node.innerHTML = bef_node + '<span id="' + val
-                                            + '" class="' + def_class 
-                                            + '" style="background-color:yellow">' + sel.toString() 
-                                            + '</span>' + end_node;
-                            e.parentElement.replaceChild(node, elem);
-                            document.getElementById(val).addEventListener('mouseenter', function(event) {enter(event, this.id)}, false);
-                            document.getElementById(val).addEventListener('mouseleave', function(event) {leave(event, this.id)}, false);
-                        }
-                    }
+    sel = window.getSelection();
+    if (storageCache.can_exec && sel.toString().length > 0) {
+        var sel_r = sel.getRangeAt(0)
+        try {
+            var elements = sel_r.commonAncestorContainer.getElementsByTagName("*");
+        } catch {
+            var e = sel_r.startContainer;
+            for (var elem of e.parentElement.childNodes) {
+                if (e.isSameNode(elem) && !Array.from(e.parentElement.classList).includes(def_class)) {
+                    var bef_node = e.textContent.slice(0, sel_r.startOffset);
+                    var end_node = e.textContent.slice(sel_r.endOffset);
+                    var node = document.createElement('span');
+                    var val = (new Date().valueOf()).toString();
+                    node.innerHTML = bef_node + '<span id="' + val
+                        + '" class="' + def_class
+                        + '" style="background-color:yellow">' + sel.toString()
+                        + '</span>' + end_node;
+                    e.parentElement.replaceChild(node, elem);
+                    document.getElementById(val).addEventListener('mouseenter', function () { enter(this.id) }, false);
+                    document.getElementById(val).addEventListener('mouseleave', function () { leave() }, false);
                 }
             }
         }
@@ -36,41 +31,56 @@ document.addEventListener('mouseup', function () {
 const x_id = (new Date().valueOf()).toString();
 var timer;
 
-var enter = function(event, id) {
-    /*
-    window.clearTimeout(timer)
-    timer = window.setTimeout(function() {
-        var elem = document.getElementById(x_id)
+var enter = function (id) {
+    window.clearTimeout(timer);
+    timer = window.setTimeout(function () {
+        var elem = document.getElementById(x_id);
         var bounding = document.getElementById(id).getBoundingClientRect();
-        elem.setAttribute("referral",id);
+        elem.setAttribute("referral", id);
         elem.style.left = (Math.round(bounding.left) + Math.round(bounding.width) + window.scrollX - 20) + "px";
         elem.style.top = (bounding.top + window.scrollY - 15) + "px";
         elem.hidden = false;
     }, 700)
-    */
 };
-var leave = function(event, id) {
-    /*
-    window.clearTimeout(timer)
-    timer = window.setTimeout(function() {
+var leave = function () {
+    window.clearTimeout(timer);
+    timer = window.setTimeout(function () {
         document.getElementById(x_id).hidden = true;
     }, 700)
-    */
 };
 
-function copyToClipBoard() {
-    var content = '';
-    var elements = document.getElementsByClassName(def_class);
-    for(var element of elements) {
-        content += element.innerText;
-    }
-    var temp = document.createElement("textarea");
-    document.body.appendChild(temp);
-    temp.value = content;
-    temp.select();
-    document.execCommand("copy");
-    document.body.removeChild(temp);
+var stopTimer = function () {
+    window.clearTimeout(timer);
 }
+
+var deleteHighlight = function (id) {
+    console.log(id);
+}
+
+function copyToClipBoard() {
+    if (storageCache.can_exec) {
+        var content = '';
+        var elements = document.getElementsByClassName(def_class);
+        for (var element of elements) {
+            content += element.innerText;
+        }
+        if (content != '') {
+            var temp = document.createElement("textarea");
+            document.body.appendChild(temp);
+            temp.value = content;
+            temp.select();
+            document.execCommand("copy");
+            document.body.removeChild(temp);
+            alert("Lines successfully copied to clipboard!");
+        }
+    }
+}
+
+document.addEventListener('keydown', (event) => {
+    if (event.ctrlKey && event.key === 'c') {
+        copyToClipBoard();
+    }
+});
 
 chrome.storage.onChanged.addListener((changes, area) => {
     if (area === 'sync' && changes.storage?.newValue) {
@@ -94,9 +104,7 @@ var node = document.createElement('div');
 node.id = x_id;
 node.className = "close";
 node.hidden = true;
-/*
-node.addEventListener('click', function() {  }, false);
-node.addEventListener('mouseenter', function() {  }, false);
-node.addEventListener('mouseleave', function(event) { leave(event, this.getAttribute("referral")) }, false);
-*/
+node.addEventListener('click', function () { deleteHighlight(this.getAttribute("referral")) }, false);
+node.addEventListener('mouseenter', function () { stopTimer() }, false);
+node.addEventListener('mouseleave', function () { leave() }, false);
 document.body.appendChild(node);
